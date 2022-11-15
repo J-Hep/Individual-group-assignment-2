4225f8a937bea50604f5a99e043d41d109afb8ee
JH <jaden.hepburn@ontariotechu.net>
2022-11-15 16:35:14 -0500

    ColliderWinCondition

--- IndividualGroupAssignment/Assets/TriggerBehavior.cs
+++ IndividualGroupAssignment/Assets/TriggerBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerBehavoiour : MonoBehaviour
+public class TriggerBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
