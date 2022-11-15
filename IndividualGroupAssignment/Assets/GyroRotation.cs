5458b922440cfe7a4cfcd3169963e9ff2b5695af
Aayush Shrestha <70073615+Hawtori@users.noreply.github.com>
2022-11-15 16:24:03 -0500

    phone shake attempt

---
+++ IndividualGroupAssignment/Assets/GyroRotation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InitializeGyro());
+    }
+
+    IEnumerator InitializeGyro()
+    {
+        Input.gyro.enabled = true;
+        yield return null;
+        Debug.Log(Input.gyro.attitude); // attitude has data now
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Rotation: " + Input.acceleration);
+        transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+    }
+}
